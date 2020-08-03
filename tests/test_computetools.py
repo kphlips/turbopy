cbcff4f5d4f290565b7f853188b286ec4112de8d
Kevin Phlips <66272415+kphlips@users.noreply.github.com>
2020-08-03 14:47:47 -0400

    Add BorisPush object to push test

--- tests/test_computetools.py
+++ tests/test_computetools.py
@@ -1,5 +1,6 @@
 "Test for the BorisPush Computetool in turbopy/computetools.py"
 
+import numpy as np
 from turbopy.computetools import BorisPush
 from turbopy.core import Simulation
 
@@ -14,6 +15,14 @@ def test_init_should_create_object():
 
 def test_push():
     """Tests the functionality of the push method"""
-    push_example = BorisPush(Simulation({"type": "BorisPush"}),
-                             {"type": "BorisPush"})
+    input_data = {"Clock": {"start_time": 0, "end_time": 1, "dt": 0.1},
+                  "type": "BorisPush",
+                  "Grid": {"N": 10, "min": 20, "max": 30},
+                  "PhysicsModules": {}}
+    owner = Simulation(input_data)
+    owner.prepare_simulation()
+    push_example = BorisPush(owner, input_data)
+    arr = np.ndarray(shape=(2, 2), dtype=float)
+    arr.fill(1)
+    push_example.push(arr, arr, 1, 1, arr, arr)
     assert True
