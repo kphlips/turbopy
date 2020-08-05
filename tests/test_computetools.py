cc3a49914d7fd5cc7bb713c302df0ac60efe0617
Kevin Phlips <66272415+kphlips@users.noreply.github.com>
2020-08-05 12:41:46 -0400

    Add push test with nonzero E and B values

--- tests/test_computetools.py
+++ tests/test_computetools.py
@@ -15,7 +15,7 @@ def test_init_should_create_object():
 
 def test_push():
     """Tests the functionality of the push method"""
-    input_data = {"Clock": {"start_time": 0, "end_time": 1, "dt": 1e-9},
+    input_data = {"Clock": {"start_time": 0, "end_time": 100, "dt": 1e-9},
                   "type": "BorisPush",
                   "Grid": {"N": 10, "min": 20, "max": 30},
                   "PhysicsModules": {}}
@@ -26,12 +26,20 @@ def test_push():
     mass = 1.6726e-27
     E = np.zeros(3)
     B = np.zeros(3)
-    x0 = np.array([[0, 0, 0.0]], dtype=np.float)
-    v0 = np.array([[0, 0, 3.0e6]], dtype=np.float)
-    p0 = mass * v0
-    dt = 1e-9
-    Nt = 10
-    xfinal = v0 * Nt * dt
-    for i in range(Nt):
-        push_example.push(x0, p0, charge, mass, E, B)
-    assert np.allclose(x0, xfinal, rtol=1.e-4)
+    x_i = np.array([[0, 0, 0.0]], dtype=np.float)
+    v_i = np.array([[0, 0, 3.0e6]], dtype=np.float)
+    p_i = mass * v_i
+    clock = input_data["Clock"]
+    N = 10
+    xfinal = v_i * N * clock["dt"]
+    for i in range(N):
+        push_example.push(x_i, p_i, charge, mass, E, B)
+    assert np.allclose(x_i, xfinal, rtol=1e-4)
+    x_i = np.array([[0, 0, 0.0]], dtype=np.float)
+    p_i = 0 * v_i
+    E = np.array([[0, 0, 1e100]], dtype=np.float)
+    B = np.array([[0, 0, 1e100]], dtype=np.float)
+    for i in range(N):
+        push_example.push(x_i, p_i, charge, mass, E, B)
+    check = np.array([[0, 0, 3.0]], dtype=np.float)
+    assert np.allclose(x_i, check, rtol=1e-3)
