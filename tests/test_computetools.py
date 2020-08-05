2032e758d4cd6ded3a2597f1ba60a65bc34277b3
kphlips <66272415+kphlips@users.noreply.github.com>
2020-08-05 17:43:45 -0400

    Update tests/test_computetools.py
    Co-authored-by: Steve Richardson <arichar6@gmail.com>

--- tests/test_computetools.py
+++ tests/test_computetools.py
@@ -12,9 +12,10 @@ def test_init_should_create_object():
     assert init_example.c2 == 2.9979e8 ** 2
 
 
-def test_push():
-    """Tests the functionality of the push method"""
-    input_data = {"Clock": {"start_time": 0, "end_time": 100, "dt": 1e-9},
+def test_push_with_no_fields():
+    """Tests the functionality of the push method, when E=B=0"""
+    input_data = {"Clock":
+                      {"start_time": 0, "end_time": 100, "dt": 1e-9},
                   "type": "BorisPush",
                   "Grid": {"N": 10, "min": 20, "max": 30},
                   "PhysicsModules": {}}
@@ -26,22 +27,39 @@ def test_push():
     E = np.zeros(3)
     B = np.zeros(3)
     x_i = np.array([[0, 0, 0.0]], dtype=np.float)
-    v_i = np.array([[0, 0, 3.0e6]], dtype=np.float)
+    v_i = np.array([[0, 0, 3.0e2]], dtype=np.float)
     p_i = mass * v_i
     clock = input_data["Clock"]
     N = 10
-    xfinal = v_i * N * clock["dt"]
+    x_final = v_i * N * clock["dt"]
     for i in range(N):
         push_example.push(x_i, p_i, charge, mass, E, B)
-    assert np.allclose(x_i, xfinal, rtol=1e-4)
+    assert np.allclose(x_i, x_final)
+
+
+def test_push_with_crossed_fields():
+    """Tests the functionality of the push method for E, B, not 0"""
+    input_data = {
+        "Clock": {"start_time": 0, "end_time": 100, "dt": 1e-9},
+        "type": "BorisPush",
+        "Grid": {"N": 10, "min": 20, "max": 30},
+        "PhysicsModules": {}}
+    owner = Simulation(input_data)
+    owner.prepare_simulation()
+    push_example = BorisPush(owner, input_data)
+    charge = 1.6022e-19
+    mass = 1.6726e-27
+    N = 10
     x_i = np.array([[0, 0, 0.0]], dtype=np.float)
-    p_i = 0 * v_i
-    E = np.array([[0, 0, 10]], dtype=np.float)
+    p_i = np.array([[0, 0, 0.0]], dtype=np.float)
+    E = np.array([[10, 0, 0]], dtype=np.float)
     B = np.array([[0, 0, 10]], dtype=np.float)
     for i in range(N):
         push_example.push(x_i, p_i, charge, mass, E, B)
-    check = np.array([[0, 0, 3.0]], dtype=np.float)
-    assert np.allclose(x_i, check, rtol=3e-2)
+    x_final = np.array([[2.20028479e-09, -1.04482737e-08, 0]])
+    assert np.allclose(x_i, x_final)
+    p_final = np.array([[0.47183691, -1.88168585,  0]]) * mass
+    assert np.allclose(p_i / mass, p_final / mass)
 
 
 @pytest.fixture
